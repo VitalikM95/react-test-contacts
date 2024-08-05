430787f31f9b58d2d07094495296932b0f973bf0
Vitalik <marychvitalik95@gmail.com>
2024-08-05 17:45:24 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/react-test-contacts",
   server: {
     proxy: {
       "/api": {
-        target: "https://cors-anywhere.herokuapp.com/corsdemo",
+        target:
+          "https://cors-anywhere.herokuapp.com/https://live.devnimble.com/api/v1",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
