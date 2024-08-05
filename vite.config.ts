4c1d76c09cd1457ffaf0a6c6acb54d150ad7017f
Vitalik <marychvitalik95@gmail.com>
2024-08-05 18:58:20 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target:
-          "https://cors-anywhere.herokuapp.com/corsdemo/https://live.devnimble.com/api/v1",
+        target: "https://proxy.cors.sh/https://live.devnimble.com/api/v1",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
