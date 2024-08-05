23bc3d9f974cb68ba45baea491d29fd67162c8d4
Vitalik <marychvitalik95@gmail.com>
2024-08-05 17:02:13 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-test-contacts/">
       <Provider store={store}>
         <App />
       </Provider>
